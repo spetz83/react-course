0cf686592aa53275afedec5300c2fbaaebac8d7a
Tom Etzel <thomas.etzel@gmail.com>
2019-02-10 21:24:20 -0600

    added axios and configured

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID e0cf7f132d1ad66df771df9f4ff62a5be8d123f56ec142dad5ca3afb70f1e98c"
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,10 +5,15 @@ class SearchBar extends Component {
     term: ""
   };
 
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
