5f6d0fc1428b74d3dc5fc5a5fdd3e101f9dec129
Tom Etzel <tom.etzel@activeradar.com>
2019-02-12 19:24:59 -0600

    complete

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization:
-      "Client-ID e0cf7f132d1ad66df771df9f4ff62a5be8d123f56ec142dad5ca3afb70f1e98c"
-  }
-});
--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from "react";
-import unsplash from "../api/unsplash";
-
-import SearchBar from "./SearchBar";
-import ImageList from "./ImageList";
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
 class App extends Component {
-  state = {
-    images: []
-  };
-
-  onSearchSubmit = async term => {
-    const response = await unsplash.get("/search/photos", {
-      params: { query: term }
-    });
-    this.setState({ images: response.data.results });
-    console.log(this.state.images);
-  };
-
   render() {
     return (
-      <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images} />
+      <div className="ui container grid">
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+          <div className="column eight wide">
+            <SongDetail />
+          </div>
+        </div>
       </div>
     );
   }
