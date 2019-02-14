1a0141163af3f21cc7f7bc915aff70025d67b26f
Tom Etzel <tom.etzel@activeradar.com>
2019-02-14 10:16:16 -0600

    finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,25 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
 
 // Action Creator
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
   // Return an action
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
